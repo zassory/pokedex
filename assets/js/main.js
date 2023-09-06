54352f17f7733051661ece93a489cf94b4b5848f
Nicolas Caceres <nicolas.programador@gmail.com>
2023-09-05 21:30:00 -0300

    estilos listos

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+const btnFilter = document.querySelector('.icon-filter');
+
+btnFilter.addEventListener('click', ()=> {
+    const containerFilter = document.querySelector('.container-filters');
+
+    containerFilter.classList.toggle('active');
+});
